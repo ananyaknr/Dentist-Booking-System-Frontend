0b5d77d54aa0a2e2ad14cd996e30ecb850098c41
Ananya <jaimai2006@gmail.com>
2025-03-19 17:00:17 +0700

    initial setup based on carRentalApp

---
+++ src/components/TopMenu.tsx
@@ -0,0 +1,35 @@
+import styles from './topmenu.module.css'
+import Image from 'next/image';
+import TopMenuItem from './TopMenuItem';
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../app/api/auth/[...nextauth]/authOptions';
+import Link from 'next/link';
+
+export default async function TopMenu(){
+
+    const session = await getServerSession(authOptions)
+
+    return(
+        <div className={styles.menucontainer}>
+            <Image src={'/img/logo.png'} className={styles.logoimg} alt='logo' width={0} height={0} sizes='100vh'></Image>
+            <TopMenuItem title='Select Car' pageRef='/car'/>
+            <TopMenuItem title='Reservations' pageRef='/reservations'></TopMenuItem>
+            <TopMenuItem title='About' pageRef='/about'></TopMenuItem>
+
+            <div className='flex flex-row absolute right-0 h-full'>
+            <TopMenuItem title='Cart' pageRef='/cart'/>
+            {
+                session ? 
+                    <Link href="/api/auth/signout">
+                    <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>
+                        Sign-Out of {session.user?.name} 
+                    </div></Link>
+                : <Link href="/api/auth/signin">
+                    <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>
+                        Sign-In
+                    </div></Link>
+            }
+            </div>
+        </div>
+    );
+}
